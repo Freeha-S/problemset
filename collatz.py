be850cd4547cb06ac3fffb7b4f6af7e98d668378
Freeha-S <freha.gretb@gmail.com>
2019-02-06 21:39:22 +0000

    4th problem solved

--- collatz.py
+++ collatz.py
@@ -5,3 +5,13 @@
 #it by three and add one. Have the program end if the current value is one.
 #Please enter a positive integer: 10
 #10 5 16 8 4 2 1
+
+i= int(input("Enter the Positive integer:"))
+
+while i > 1:
+    if i%2 == 0:
+        i= i//2
+        print(i)
+    else:
+        i=i*3+1
+        print(int(i))
