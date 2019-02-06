b18c96e701e209a1326e0b030887835da9e386dc
Freeha-S <freha.gretb@gmail.com>
2019-02-06 21:10:11 +0000

    third problem solved

--- divisors.py
+++ divisors.py
@@ -1,2 +1,10 @@
 #prints all numbers between 1,000 and 10,000 
 # that are divisible by 6 but not 12.
+
+i=1000
+
+while i<10000:
+    if i % 6 == 0:
+        if i % 12 !=0:
+            print (i)
+    i= i + 1
