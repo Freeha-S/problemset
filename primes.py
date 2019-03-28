7be4420fb687fe448af1496103d0eeda4bff00c7
Freeha-S <freha.gretb@gmail.com>
2019-03-28 21:48:00 +0000

    Fifth problem update

--- primes.py
+++ primes.py
@@ -5,25 +5,32 @@
 
 
 def prime(num):
-   # set a variable j value to 2
-   j= 2
-   # message with default value a prime number
-   message = "a Prime Number"
-   #start a loop for divide by 2 until num-1
-   while j < num-1:
-      # if number is devisible by the j
-      if num % j == 0:
-         #change the value of message to not prime
-         message = "not a Prime Number"
-         # assign the num value to j to stop loop
-         j=num
-      else:
-         #increase the value of j
-         j = j + 1
-   #return the message 
-   return message
 
-# input a number
-num = int(input("Enter the Positive integer:"))
-# print i and the call prime and get message if prime or not
-print(num," is ",prime(num))
+   if num <= 1: #if number is 0 and 1 its not prime so return the messaage ont prime
+      return "not a Prime Number"
+   
+   j= 2 # set a variable j value to 2
+   message = "a Prime Number"  # message with default value a prime number
+  
+   while j < num-1:  #start a loop start from 2 until num-1
+      if num % j == 0: # if number is devisible by the j
+         message = "not a Prime Number"  #change the string value of message to not prime
+         break # assign the num value to j to stop loop
+      else:
+         j = j + 1  #increase the value of j
+   return message #return the message 
+
+# input a value from user and assign that value to variable num
+while True: # loop for getting valid input
+    try:
+        num = int(input("Enter the Positive integer:"))
+    except ValueError: # exception if not an integer input
+        print(" Not a valid input ")
+        continue
+    if num < 0: # check if the input is less than 0
+        print("Number must not be negative.")
+        continue
+    else:
+       break #if number is positive than exit the loop
+
+print(num," is ",prime(num)) # print num and the call prime function and get message if prime or not
