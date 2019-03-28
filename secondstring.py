32bc0325229a24a6aecc3d6e93cf5dc0a8f0fb8a
Freeha-S <freha.gretb@gmail.com>
2019-03-28 22:55:39 +0000

    Sixth problem update

--- secondstring.py
+++ secondstring.py
@@ -1,18 +1,11 @@
-# takes a user input string and outputs every second word.
+# This programme takes a user input string and outputs every second word.
 #$ python secondstring.py
 #Please enter a sentence: The quick brown fox jumps over the lazy dog.
 #The brown jumps the dog
 #@ Freha
 
-# Input a message
-message = input("Please enter a sentance:")
-#split the message into words get list of words
-words = message.split()
-
-# get the numbers of words
-length = len(message.split())
-
-#print the every second word start from 0 to length increment by 2
-for x in range (0,length,2):
-   
-    print(words[x], end=" ")
+message = input("Please enter a sentance:") # Input a string and assign it to message
+words = message.split() #split the message into words get list of words
+length = len(message.split()) # get the numbers of words
+for x in range (0,length,2): #print the every second word start from 0 index to length increment by 2
+     print(words[x], end=" ")
